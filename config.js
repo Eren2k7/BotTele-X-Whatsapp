d37eaea9e9490c0dc5322f1a496cab17f5177789
Eren2k7 <petersergio42@gmail.com>
2025-09-01 05:49:50 +0100

    config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  BOT_TOKEN: "7440325340:AAGXTCGCkS90mV-2ltF9rCWGloqWX2isrzA", // Token bot Telegram
-  OWNER_ID: "6111586845", // ID pemilik bot
+  BOT_TOKEN: "8314788198:AAHRmvNz9Kh2nQUE-qYV6gtyKMxGChEW6AA", // Token bot Telegram
+  OWNER_ID: "7799545092", // ID pemilik bot
   allowedGroupIds: [], // ID grup yang diizinkan
 };
